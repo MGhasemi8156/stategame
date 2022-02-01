33428ad7f152ff1a49e0f2c14b72c288c7f5cd05
Mohsen Ghasemi <mohsenghasemi8156@gmail.com>
2022-02-01 20:10:23 +0330

    Change potions apreance

--- src/potion.c
+++ src/potion.c
@@ -24,6 +24,9 @@ void end_unlimited_rebirth(int target_side, int lands_n, Land lands[]);
 void increase_rebirth_rate(int target_side, int lands_n, Land lands[]);
 void end_increase_rebirth_rate(int target_side, int lands_n, Land lands[]);
 
+int get_type_duration(int type);
+Uint32 get_type_color(int type);
+
 void add_potion(int* potions_n, Potion potions[], int lands_n, Land lands[]) {
     if (*potions_n < MAX_POTIONS) {
         for (int i = 0; i < lands_n; i++) {
@@ -39,9 +42,9 @@ void add_potion(int* potions_n, Potion potions[], int lands_n, Land lands[]) {
                     temp.x = lands[i].barrack_x + rx * rand_factor;
                     temp.y = lands[i].barrack_y + ry * rand_factor;
                     
-                    temp.type = 4; // TODO change randonm value
+                    temp.type = rand()%4 + 1;
                     temp.target_side = -1;
-                    temp.till_end = 600; // TODO change
+                    temp.till_end = get_type_duration(temp.type); // TODO change
                     
                     potions[*potions_n] = temp;
                     *potions_n += 1;
@@ -62,10 +65,27 @@ void apply_potions(SDL_Renderer* Renderer, int postions_n, Potion potions[],
             vy[k] = potions[i].y + sin(PI/(POTION_SIDES / 2) * k) * POTION_R;
         }
         if (potions[i].target_side == -1) {
-            filledPolygonColor(Renderer, vx, vy, POTION_SIDES, 0xaaff0000);
+            filledPolygonColor(Renderer, vx, vy, POTION_SIDES, get_type_color(potions[i].type));
         }
         else {
-            filledPolygonColor(Renderer, vx, vy, POTION_SIDES, 0x55ff0000);
+            // draw a boudary for owner
+            Sint16 vx_owner[POTION_SIDES];  
+            Sint16 vy_owner[POTION_SIDES];
+            for (int k = 0; k < POTION_SIDES; k++) {
+                vx_owner[k] = potions[i].x + cos(PI/(POTION_SIDES / 2) * k) * (POTION_R + 6);
+                vy_owner[k] = potions[i].y + sin(PI/(POTION_SIDES / 2) * k) * (POTION_R + 6);
+            }
+            
+            filledPolygonColor(Renderer, vx_owner, vy_owner, POTION_SIDES, get_side_normal_color(potions[i].target_side));
+            filledPolygonColor(Renderer, vx, vy, POTION_SIDES, 0xffffffff);
+            
+            filledPolygonColor(Renderer, vx, vy, POTION_SIDES, get_type_color(potions[i].type) - 0x80000000);
+            
+            // draw timer 
+            char potion_timer[5];
+            sprintf(potion_timer, "%d", potions[i].till_end/60 + 1);
+            stringRGBA(Renderer, potions[i].x - 4, potions[i].y - 4, potion_timer, 0, 0, 0, 255);
+
             potions[i].till_end -= 1;
             if (potions[i].till_end > 0) {
                 switch (potions[i].type) {
@@ -166,5 +186,31 @@ void end_increase_rebirth_rate(int target_side, int lands_n, Land lands[]) {
         if (lands[i].side == target_side) {
             lands[i].rebirth_rate = REBIRTH_RATE;
         }
+    }
+}
+
+Uint32 get_type_color(int type) {
+    switch (type) {
+        case 1: 
+            return 0xffddff00;
+        case 2:
+            return 0xff00ffa2;
+        case 3:
+            return 0xffff00d4;
+        case 4:
+            return 0xff00ccfd;
+    }
+}
+
+int get_type_duration(int type) {
+    switch (type) {
+        case 1:
+            return STOP_ENEMY_DURATION;
+        case 2:
+            return INCREASE_VELOCITY_DURATION;
+        case 3:
+            return UNLIMITED_REBIRTH_DURATION;
+        case 4:
+            return INCREASE_REBIRTH_RATE_DURATION;
     }
 }
